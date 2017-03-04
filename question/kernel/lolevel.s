5d50cfde3fd20ab536b04c83ceb30b7e7f436771
TheObnoxiousFrog <jl15216@my.bristol.ac.uk>
2017-03-04 15:04:04 +0000

    30 percent

--- question/kernel/lolevel.s
+++ question/kernel/lolevel.s
@@ -4,31 +4,58 @@
  */
 
 .global lolevel_handler_rst
-.global lolevel_handler_irq
 .global lolevel_handler_svc
-
+.global lolevel_handler_irq
+	
 lolevel_handler_rst: bl    int_init                @ initialise interrupt vector table
 
                      msr   cpsr, #0xD2             @ enter IRQ mode with IRQ and FIQ interrupts disabled
                      ldr   sp, =tos_irq            @ initialise IRQ mode stack
+
                      msr   cpsr, #0xD3             @ enter SVC mode with IRQ and FIQ interrupts disabled
                      ldr   sp, =tos_svc            @ initialise SVC mode stack
 
+                     sub   sp, sp, #68             @ initialise dummy content
+
+                     mov   r0, sp                  @ set high - level C function arg. = SP
                      bl    hilevel_handler_rst     @ invoke high-level C function
-                     b     .                       @ halt
 
-lolevel_handler_irq: sub   lr, lr, #4              @ correct return address
-                     stmfd sp!, { r0-r3, ip, lr }  @ save    caller-save registers
-
-                     bl    hilevel_handler_irq     @ invoke high-level C function
-
-                     ldmfd sp!, { r0-r3, ip, lr }  @ restore caller-save registers
-                     movs  pc, lr                  @ return from interrupt
+                     ldmia sp!, { r0, lr }         @ load USR mode PC and CPSR
+                     msr   spsr, r0                @ set  USR mode CPSR
+                     ldmia sp, { r0-r12, sp, lr }^ @ load USR mode register
+                     add   sp, sp, #60             @ update SVC mode SP
+                     movs  pc, lr                  @ Switches back to user mode
 
 lolevel_handler_svc: sub   lr, lr, #0              @ correct return address
-                     stmfd sp!, { r0-r3, ip, lr }  @ save    caller-save registers
+                     sub   sp, sp, #60             @ update SVC mode stack
+                     stmia sp, { r0-r12, sp, lr }^ @ store USR mode stack
+                     mrs   r0, spsr                @ get USR CPSR
+                     stmdb sp!, { r0, lr }         @ store USR PC and CPSR
 
-                     bl    hilevel_handler_svc     @ invoke high-level C function
+                     mov   r0, sp                  @ set high-level C function arg. = SP
+                     ldr   r1, [ lr, #-4 ]         @ load svc instruction
+                     bic   r1, r1, #0xFF000000     @ set high-level C function arg. = svc immediate
+                     bl    hilevel_handler_svc     @ invoke high level C function
 
-                     ldmfd sp!, { r0-r3, ip, lr }  @ restore caller-save registers
-                     movs  pc, lr                  @ return from interrupt 
+                     ldmia sp!, { r0, lr }         @ load USR mode PC and CPSR
+                     msr   spsr, r0                @ set  USR mode CPSR
+                     ldmia sp, { r0-r12, sp, lr }^ @ load USR mode register
+                     add   sp, sp, #60             @ update SVC mode SP
+                     movs  pc, lr                  @ Switches back to user mode
+
+
+lolevel_handler_irq: sub   lr, lr, #4              @ correct return address
+                     sub   sp, sp, #60              @ update IRQ mode stack
+                     stmia sp, { r0-r12, sp, lr }^ @ store USR mode stack
+                     mrs   r0, spsr                @ get USR CPSR
+                     stmdb sp!, { r0, lr }         @ store USR PC and CPSR
+        
+                     mov   r0, sp                  @ set high-level C function arg. = SP
+                     bl    hilevel_handler_irq     @ invoke high level C function
+
+                     ldmia sp!, { r0, lr }         @ load USR mode PC and CPSR
+                     msr   spsr, r0                @ set  USR mode CPSR
+                     ldmia sp, { r0-r12, sp, lr }^ @ load USR mode register
+                     add   sp, sp, #60             @ update IRQ mode SP
+                     movs  pc, lr                  @ Switches back to user mode
+
